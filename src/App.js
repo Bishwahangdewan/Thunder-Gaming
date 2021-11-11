3c575c07096aca2ccab6621c24be2c48c2c32372
Bishwahang Dewan <bishwahangdewan123@gmail.com>
2021-11-11 18:33:45 +0530

    more updates: installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-import Game1 from '../../assets/game1.jpg';
-import Game2 from '../../assets/game2.jpg';
-
 const MenuItem = ({ title, img }) => (
     <div style={{ backgroundImage: `url(${img})` }} className="menu-item">
         <div className="content">
