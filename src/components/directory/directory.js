539e75f7e2cd6de04d97b34502d0ab1f37de8260
Bishwahang Dewan <bishwahangdewan123@gmail.com>
2021-11-12 18:07:49 +0530

    update

--- src/components/directory/directory.js
+++ src/components/directory/directory.js
@@ -9,8 +9,8 @@ class Directory extends React.Component {
 
         this.state = {
             sections: [
-                { id: 0, title: "Newest", img: "http://localhost:3000/static/media/game1.de8fcacc.jpg" },
-                { id: 1, title: "Most Popular", img: "http://localhost:3000/static/media/game2.a8258650.jpg" }
+                { id: 0, title: "Newest", img: "http://localhost:3000/static/media/game1.de8fcacc.jpg", linkto: '/newest' },
+                { id: 1, title: "Most Popular", img: "http://localhost:3000/static/media/game2.a8258650.jpg", linkto: '/mostpopular' }
             ]
         }
     }
@@ -18,8 +18,8 @@ class Directory extends React.Component {
     render() {
         return (
             <div className="directory-menu">
-                {this.state.sections.map(section => (
-                    <MenuItem key={section.id} title={section.title} img={section.img} />
+                {this.state.sections.map(({ id, ...othersectionprops }) => (
+                    <MenuItem key={id} {...othersectionprops} />
                 ))}
             </div>
         )
--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, img }) => (
-    <div style={{ backgroundImage: `url(${img})` }} className="menu-item">
-        <div className="content">
-            <h1 className="title">{title}</h1>
-            <span className="subtitle">SHOP NOW</span>
-        </div>
-    </div>
-)
+
+const MenuItem = ({ title, img, linkto }) => {
+    const navigate = useNavigate();
+
+    return (
+        <div style={{ backgroundImage: `url(${img})` }} className="menu-item">
+            <div className="content">
+                <h1 className="title">{title}</h1>
+                <span className="subtitle" onClick={() => navigate(`${linkto}`)}>SHOP NOW</span>
+            </div>
+        </div >
+    )
+}
 
 
 export default MenuItem;
