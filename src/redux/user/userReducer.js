1ca5627b998ee72059f568cec9a27f8e8541ebbf
Bishwahang Dewan <bishwahangdewan123@gmail.com>
2021-11-29 18:14:18 +0530

    Redux Setup Completed

---
+++ src/redux/user/userReducer.js
@@ -0,0 +1,18 @@
+const INITIAL_STATE = {
+    currentUser: null
+}
+
+const userReducer = (state = INITIAL_STATE, action) => {
+    switch (action.payload) {
+        case '':
+            return {
+                ...state,
+                currentUser: action.payload
+            };
+
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
